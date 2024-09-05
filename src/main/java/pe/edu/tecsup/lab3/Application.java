80f4b2f911c53edca9b7ad9c9ba70c7f41c8ecb1
Andres Huancahuari <ramdom@abc>
2024-09-05 16:12:50 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/lab3/Application.java
@@ -0,0 +1,5 @@
+package pe.edu.tecsup.lab3;
+
+public class Application {
+    //to do
+}
---
+++ src/main/java/pe/edu/tecsup/lab3/Main.java
@@ -0,0 +1,7 @@
+package pe.edu.tecsup.lab3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
